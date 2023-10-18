a0a754a5d7ef958c86f84ad588ddde63c66b8160
dean06001 <dean06001@E-PW0234KC>
2023-10-18 10:11:03 +0200

    Add project files.

---
+++ inlämnings uppgift 4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlämnings_uppgift_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 200; i > 179; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1000; i < 1401; i += 50)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
